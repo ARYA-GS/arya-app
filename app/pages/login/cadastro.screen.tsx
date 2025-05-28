2cc8bfdda4710e2723e6299ad70c06c90139a956
Vitor Santos <vitorsacz@gmail.com>
2025-05-27 21:54:06 -0300

    criando telas base

---
+++ app/pages/login/cadastro.screen.tsx
@@ -0,0 +1,12 @@
+
+import { View, Text } from 'react-native'
+
+const CadastroScreen = () => {
+  return (
+    <View>
+      <Text>cadastro.screen</Text>
+    </View>
+  )
+}
+
+export default CadastroScreen
---
+++ app/pages/login/login.screen.tsx
@@ -0,0 +1,13 @@
+
+
+import { View, Text } from 'react-native'
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login.screen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
